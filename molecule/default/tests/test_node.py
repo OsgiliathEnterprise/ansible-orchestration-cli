bf4b10ca3c4f0e250a34f552dc8983f1c27db41e
Charlie Mordant <cmordant1@gmail.com>
2021-08-28 14:44:08 +0200

    all template file

--- molecule/default/tests/test_node.py
+++ molecule/default/tests/test_node.py
@@ -1,10 +1,3 @@
 """Role testing files using testinfra."""
 testinfra_hosts = ["node1.osgiliath.test"]
 
-
-def test_kubelet_active(host):
-    with host.sudo():
-        command = """service kubelet status | \
-        grep -c 'active (running)'"""
-        cmd = host.run(command)
-    assert '1' in cmd.stdout
